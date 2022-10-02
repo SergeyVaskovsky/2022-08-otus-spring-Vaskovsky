646ba6687f1e1934b08719e43a61cdedebfb6481
vaskovsky <sergey.vaskovsky@gmail.com>
2022-10-02 17:15:56 +0300

    version after review 28.09.2022

--- homework03/src/test/java/ru/otus/Homework03ApplicationTests.java
+++ homework03/src/test/java/ru/otus/Homework03ApplicationTests.java
@@ -1,12 +1,9 @@
 package ru.otus;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ImportAutoConfiguration(MessageSourceAutoConfiguration.class)
 public class Homework03ApplicationTests {
 	@Test
 	void contextLoads() {
