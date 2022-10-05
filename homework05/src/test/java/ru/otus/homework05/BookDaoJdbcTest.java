ef13d815efb06c2ce7e782a741f1963b25167238
vaskovsky <sergey.vaskovsky@gmail.com>
2022-10-05 19:55:25 +0300

    third commit

--- homework05/src/test/java/ru/otus/homework05/BookDaoJdbcTest.java
+++ homework05/src/test/java/ru/otus/homework05/BookDaoJdbcTest.java
@@ -27,12 +27,38 @@ public class BookDaoJdbcTest {
     @Autowired
     private BookDaoJdbc bookDaoJdbc;
 
+    @DisplayName("Найти книгу в БД")
+    @Test
+    void shouldFindBookById() {
+
+    }
+
+
     @DisplayName("Добавить книгу в БД")
     @Test
     void shouldInsertBook() {
         Author author = authorDaoJdbc.getById(1L);
         Genre genre = genreDaoJdbc.getById(1L);
         bookDaoJdbc.deleteById(1L);
+        Book expectedBook = new Book(1L, "Преступление и наказание", author, genre);
+        bookDaoJdbc.insert(expectedBook);
+        Book actualBook = bookDaoJdbc.getById(expectedBook.getId());
+        assertThat(actualBook).usingRecursiveComparison().isEqualTo(expectedBook);
+    }
+
+    @DisplayName("Изменить книгу в БД")
+    @Test
+    void shouldUpdateBook() {
+        Author authorOld = authorDaoJdbc.getById(1L);
+        Genre genreOld = genreDaoJdbc.getById(1L);
+        Book expectedBook = new Book(1L, "Преступление и наказание", authorOld, genreOld);
+        bookDaoJdbc.getById(expectedBook.getId());
+        bookDaoJdbc.insert(expectedBook);
+
+        Author authorNew = authorDaoJdbc.getById(2L);
+        Genre genreNew = genreDaoJdbc.getById(2L);
+
+
         Book expectedBook = new Book(1L, "Преступление и наказание", author, genre);
         bookDaoJdbc.insert(expectedBook);
         Book actualBook = bookDaoJdbc.getById(expectedBook.getId());
--- homework05/src/test/resources/data.sql
+++ homework05/src/test/resources/data.sql
@@ -3,3 +3,9 @@ VALUES (1, 'Достаевский Ф. М.');
 
 INSERT INTO GENRE(ID, NAME)
 VALUES (1, 'Триллер');
+
+INSERT INTO AUTHOR(ID, NAME)
+VALUES (2, 'Коллектив авторов');
+
+INSERT INTO GENRE(ID, NAME)
+VALUES (2, 'Научпоп');
