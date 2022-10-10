0b4383df26c02512ee04793bb791e696a23219b3
vaskovsky <sergey.vaskovsky@gmail.com>
2022-10-10 22:00:04 +0300

    equals and hashcode

--- homework05/src/main/java/ru/otus/homework05/model/Book.java
+++ homework05/src/main/java/ru/otus/homework05/model/Book.java
@@ -2,12 +2,16 @@ package ru.otus.homework05.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
 public class Book {
     private long id;
+    @EqualsAndHashCode.Exclude
     private String name;
+    @EqualsAndHashCode.Exclude
     private Author author;
+    @EqualsAndHashCode.Exclude
     private Genre genre;
 }
--- homework05/src/main/java/ru/otus/homework05/model/Genre.java
+++ homework05/src/main/java/ru/otus/homework05/model/Genre.java
@@ -2,10 +2,12 @@ package ru.otus.homework05.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
 public class Genre {
     private long id;
+    @EqualsAndHashCode.Exclude
     private String name;
 }
