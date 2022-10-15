4607075f576eda3a674b78781224612320ce73fd
vaskovsky <sergey.vaskovsky@gmail.com>
2022-10-15 19:51:52 +0300

    output is in process

--- homework05/src/main/java/ru/otus/homework05/service/OutputServiceImpl.java
+++ homework05/src/main/java/ru/otus/homework05/service/OutputServiceImpl.java
@@ -1,4 +1,20 @@
 package ru.otus.homework05.service;
 
-public class OutputServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OutputServiceImpl implements OutputService {
+
+    private OutputProvider outputProvider;
+
+    @Autowired
+    public OutputServiceImpl(OutputProvider outputProvider) {
+        this.outputProvider = outputProvider;
+    }
+
+    @Override
+    public void outputString(String message) {
+        outputProvider.getOutput().
+    }
 }
