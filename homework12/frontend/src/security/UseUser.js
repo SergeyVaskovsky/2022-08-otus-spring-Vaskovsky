bf97edd8cd4ad6986194ba73f4d550eaf35c6aaf
vaskovsky <sergey.vaskovsky@gmail.com>
2022-12-11 21:58:34 +0300

    a little improvement

--- homework12/frontend/src/security/UseUser.js
+++ homework12/frontend/src/security/UseUser.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
-import AuthService from "../service/AuthService";
 
 export default function useUser() {
 
     const [user, setUser] = useState();
 
-    const saveUser = user=> {
+    const saveUser = user => {
         localStorage.setItem('user', user);
         setUser(user);
     };
