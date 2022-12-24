0e5288feace4899c50556ed025c59dcb053d44ae
vaskovsky <sergey.vaskovsky@gmail.com>
2022-12-24 19:08:19 +0300

    some improvements

--- homework14/src/main/java/ru/otus/homework14/config/ItemProcessorConfig.java
+++ homework14/src/main/java/ru/otus/homework14/config/ItemProcessorConfig.java
@@ -1,7 +1,6 @@
 package ru.otus.homework14.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +16,21 @@ import ru.otus.homework14.service.GenreService;
 @Configuration
 @RequiredArgsConstructor
 public class ItemProcessorConfig {
-    @StepScope
     @Bean
     public ItemProcessor<Author, ru.otus.homework14.model.rdb.Author> processorAuthor(AuthorService authorService) {
         return authorService::doId;
     }
 
-    @StepScope
     @Bean
     public ItemProcessor<Genre, ru.otus.homework14.model.rdb.Genre> processorGenre(GenreService genreService) {
         return genreService::doId;
     }
 
-    @StepScope
     @Bean
     public ItemProcessor<Book, ru.otus.homework14.model.rdb.Book> processorBook(BookService bookService) {
         return bookService::doId;
     }
 
-    @StepScope
     @Bean
     public ItemProcessor<Comment, ru.otus.homework14.model.rdb.Comment> processorComment(CommentService commentService) {
         return commentService::doId;
