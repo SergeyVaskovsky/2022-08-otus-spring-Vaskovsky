72cdfb32d4288f3d1f28590767ea4de2ba2a03b3
vaskovsky <sergey.vaskovsky@gmail.com>
2022-12-25 18:25:03 +0300

    full version after review

--- homework14/src/test/java/ru/otus/homework14/config/ImportJobTest.java
+++ homework14/src/test/java/ru/otus/homework14/config/ImportJobTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
@@ -15,53 +14,51 @@ import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.otus.homework14.model.mongo.Author;
 import ru.otus.homework14.model.mongo.Book;
 import ru.otus.homework14.model.mongo.Comment;
 import ru.otus.homework14.model.mongo.Genre;
-import ru.otus.homework14.service.AuthorService;
-import ru.otus.homework14.service.BookService;
-import ru.otus.homework14.service.CommentService;
-import ru.otus.homework14.service.GenreService;
+import ru.otus.homework14.repository.AuthorRepository;
+import ru.otus.homework14.repository.BookRepository;
+import ru.otus.homework14.repository.CommentRepository;
+import ru.otus.homework14.repository.GenreRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @SpringBatchTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ImportJobTest {
-
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
-
     @Autowired
     private JobRepositoryTestUtils jobRepositoryTestUtils;
-
     @Autowired
     private MongoItemReader<Book> readerBook;
-
-    @Autowired
-    private BookService bookService;
-
-    @Autowired
-    private AuthorService authorService;
-
-    @Autowired
-    private GenreService genreService;
-
-    @Autowired
-    private CommentService commentService;
-    @Autowired
-    private ItemProcessor<Book, ru.otus.homework14.model.rdb.Book> processorBook;
-    @Autowired
-    private ItemProcessor<Author, ru.otus.homework14.model.rdb.Author> processorAuthor;
-    @Autowired
-    private ItemProcessor<Genre, ru.otus.homework14.model.rdb.Genre> processorGenre;
-    @Autowired
-    private ItemProcessor<Comment, ru.otus.homework14.model.rdb.Comment> processorComment;
-
     @Autowired
     private MongoTemplate template;
+    @Autowired
+    private CustomAuthorWriter customAuthorWriter;
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Autowired
+    private CustomGenreWriter customGenreWriter;
+    @Autowired
+    private GenreRepository genreRepository;
+    @Autowired
+    private CustomBookWriter customBookWriter;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private CustomCommentWriter customCommentWriter;
+    @Autowired
+    private CommentRepository commentRepository;
+
 
     @BeforeEach
     void clearMetaData() {
@@ -100,28 +97,51 @@ class ImportJobTest {
     }
 
     @Test
-    public void shouldConvertSuccessfully() throws Exception {
+    public void shouldCorrectSaveAuthor() {
+        Author author = new Author("1", "My name");
+        customAuthorWriter.write(List.of(author));
+        Optional<ru.otus.homework14.model.rdb.Author> a = authorRepository.findById(1L);
+        assertNotNull(a.get());
+        assertThat(a.get().getName()).isEqualTo("My name");
+    }
+
+    @Test
+    public void shouldCorrectSaveGenre() {
+        Genre genre = new Genre("2", "My favorite genre");
+        customGenreWriter.write(List.of(genre));
+        Optional<ru.otus.homework14.model.rdb.Genre> g = genreRepository.findById(1L);
+        assertNotNull(g.get());
+        assertThat(g.get().getName()).isEqualTo("My favorite genre");
+    }
+
+    @Test
+    public void shouldCorrectSaveBook() {
+        Author author = new Author("1", "My name");
+        Genre genre = new Genre("2", "My favorite genre");
+        Book book = new Book("3", "My book", author, genre);
+        customAuthorWriter.write(List.of(author));
+        customGenreWriter.write(List.of(genre));
+        customBookWriter.write(List.of(book));
+        Optional<ru.otus.homework14.model.rdb.Book> b = bookRepository.findById(1L);
+        assertNotNull(b.get());
+        assertThat(b.get().getName()).isEqualTo("My book");
+        assertThat(b.get().getAuthorId()).isEqualTo(1L);
+        assertThat(b.get().getGenreId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void shouldCorrectSaveComment() {
         Author author = new Author("1", "My name");
         Genre genre = new Genre("2", "My favorite genre");
         Book book = new Book("3", "My book", author, genre);
         Comment comment = new Comment("4", "My lovely testimonials", book);
-        ru.otus.homework14.model.rdb.Author toAuthor = processorAuthor.process(author);
-        ru.otus.homework14.model.rdb.Genre toGenre = processorGenre.process(genre);
-        ru.otus.homework14.model.rdb.Book toBook = processorBook.process(book);
-        ru.otus.homework14.model.rdb.Comment toComment = processorComment.process(comment);
-
-        assertNotNull(toAuthor);
-        assertNotNull(toGenre);
-        assertNotNull(toBook);
-        assertNotNull(toComment);
-
-        assertThat(toBook.getId()).isEqualTo(1);
-        assertThat(toBook.getName()).isEqualTo("My book");
-        assertThat(toBook.getAuthorId()).isEqualTo(1);
-        assertThat(toBook.getGenreId()).isEqualTo(1);
-
-        assertThat(toComment.getId()).isEqualTo(1);
-        assertThat(toComment.getDescription()).isEqualTo("My lovely testimonials");
-        assertThat(toComment.getBookId()).isEqualTo(1);
+        customAuthorWriter.write(List.of(author));
+        customGenreWriter.write(List.of(genre));
+        customBookWriter.write(List.of(book));
+        customCommentWriter.write(List.of(comment));
+        Optional<ru.otus.homework14.model.rdb.Comment> c = commentRepository.findById(1L);
+        assertNotNull(c.get());
+        assertThat(c.get().getDescription()).isEqualTo("My lovely testimonials");
+        assertThat(c.get().getBookId()).isEqualTo(1L);
     }
 }
