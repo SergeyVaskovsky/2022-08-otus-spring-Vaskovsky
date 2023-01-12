0a71f75ca469c50b698fabfe7a0fbf1424a17112
vaskovsky <sergey.vaskovsky@gmail.com>
2023-01-12 22:35:24 +0300

    clear project

--- homework16/src/main/java/ru/otus/homework16/repository/AuthorRepository.java
+++ homework16/src/main/java/ru/otus/homework16/repository/AuthorRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.homework16.model.Author;
 
-import java.util.List;
-
 
 @RepositoryRestResource(path = "authors")
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    List<Author> findAll();
 }
--- homework16/src/main/java/ru/otus/homework16/repository/GenreRepository.java
+++ homework16/src/main/java/ru/otus/homework16/repository/GenreRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.homework16.model.Genre;
 
-import java.util.List;
-
 @RepositoryRestResource(path = "genres")
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-    List<Genre> findAll();
 }
