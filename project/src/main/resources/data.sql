723647d9e8c8db0e3058517a641dc3e5ae1448e8
vaskovsky <sergey.vaskovsky@gmail.com>
2023-02-26 15:52:36 +0300

    add test data and description

--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -34,21 +34,37 @@ VALUES (1, 1),
        (3, 3),
        (3, 4);
 
-/*INSERT INTO POEM(TITLE)
-VALUES ('Мое первое стихотворение');
+INSERT INTO POEM(TITLE)
+VALUES ('Парус');
+
+INSERT INTO POEM(TITLE)
+VALUES ('Кузнечик');
 
 INSERT INTO COMMENT(MODERATED, PUBLISH_TIME, TEXT, POEM_ID, ROOT_COMMENT_ID, USER_ID)
-VALUES (FALSE, NULL, 'Очень классное стихотворение', 1, NULL, 1);
+VALUES (TRUE, '2023-02-26', 'Очень классное стихотворение', 1, NULL, 1);
 
 INSERT INTO COMMENT(MODERATED, PUBLISH_TIME, TEXT, POEM_ID, ROOT_COMMENT_ID, USER_ID)
-VALUES (FALSE, NULL, 'Вообще ерунда', 1, NULL, 1);
+VALUES (TRUE, '2023-02-26', 'Вообще ерунда', 1, NULL, 1);
 
 INSERT INTO POEM_ELEMENT(POEM_ID)
 VALUES (1),
-       (1);
-
-INSERT INTO POEM_PICTURE_ELEMENT(PICTURE, SCALE, ID)
-VALUES ('\xDEADBEEF', 50, 1);
+       (2),
+       (2);
 
 INSERT INTO POEM_TEXT_ELEMENT(CONTENT, ID)
-VALUES ('Текст стихотворения', 2);*/
+VALUES ('Белеет парус одинокий
+В тумане моря голубом
+Что ищет он в краю далеком,
+Что бросил он в краю родном.', 1);
+
+INSERT INTO POEM_TEXT_ELEMENT(CONTENT, ID)
+VALUES ('В траве сидел кузнечик
+В траве сидел кузнечик
+Совсем как огуречик
+Зелененький он был', 2);
+
+INSERT INTO POEM_TEXT_ELEMENT(CONTENT, ID)
+VALUES ('Но тут пришла лягушка
+Прожорливое брюшко
+Прожорливое брюшко
+И съела кузнеца', 3);
